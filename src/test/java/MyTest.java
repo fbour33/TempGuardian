c16c40350db7ba286bc710d2172e0e531d9d9c96
BOUR Florian <Florian.Bour@bordeaux-inp.fr>
2023-12-21 10:58:22 +0100

    Project Skeleton

---
+++ src/test/java/MyTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MyTest {
+	@BeforeEach
+	void setUp() {
+
+	}
+
+	@Test
+	void a_simple_unit_test() {
+		int value = 2 + 2;
+
+		assertEquals(4, value);
+	}
+}
