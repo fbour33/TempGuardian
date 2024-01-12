7c07b2720320167a5c4bda1ae8963acd882ae585
BOUR Florian <Florian.Bour@bordeaux-inp.fr>
2024-01-12 09:22:56 +0100

    Configuration system unit test finished

--- src/test/java/ConfigurationSystemTest.java
+++ src/test/java/ConfigurationSystemTest.java
@@ -1,11 +1,11 @@
 import org.junit.jupiter.api.Test;
-import tempGuardian.ConfigurationSystem;
-import tempGuardian.IUser;
+import tempGuardian.*;
 
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 public class ConfigurationSystemTest {
 
@@ -30,6 +30,16 @@ public class ConfigurationSystemTest {
     }
 
     @Test
+    void get_one_user_address() throws FileNotFoundException {
+        ConfigurationSystem configurationSystem = new ConfigurationSystem("data/test/one_user_with_one_address.csv");
+        IUser user = mock(User.class);
+        when(user.getName()).thenReturn("StellarJourney92");
+        ArrayList<IAddress> userAddress = configurationSystem.getUserAddresses(user);
+        assertEquals(userAddress.size(), 1);
+        assertEquals(userAddress.get(0).getLocation(), "61 Chemin Challet 59800 Lille");
+    }
+
+    @Test
      void adding_one_user_with_one_address() throws FileNotFoundException {
         ConfigurationSystem configurationSystem = new ConfigurationSystem("data/test/one_user_with_one_address.csv");
         ArrayList<IUser> userList = configurationSystem.getAllUsers();
@@ -42,6 +52,20 @@ public class ConfigurationSystemTest {
         assertEquals(user.getAddresses().get(0).getThresholds().get(0).getMaxThreshold(), 8);
     }
 
+    boolean isInAddressList(ArrayList<IAddress> addresses, String location){
+        for(IAddress address: addresses)
+            if(address.getLocation().equals(location))
+                return true;
+        return false;
+    }
+
+    ArrayList<IWeatherThreshold> getThresholdsByLocation(ArrayList<IAddress> addresses, String location){
+        for(IAddress address: addresses)
+            if(address.getLocation().equals(location))
+                return address.getThresholds();
+        return null;
+    }
+
     @Test
     void adding_one_user_with_multiple_addresses() throws FileNotFoundException {
         ConfigurationSystem configurationSystem = new ConfigurationSystem("data/test/one_user_with_multiple_addresses.csv");
@@ -51,13 +75,15 @@ public class ConfigurationSystemTest {
         IUser user = userList.get(0);
         assertEquals(user.getName(), "StellarJourney92");
         assertEquals(user.getAddresses().size(), 2);
-        assertEquals(user.getAddresses().get(1).getLocation(), "61 Chemin Challet 59800 Lille");
-        assertEquals(user.getAddresses().get(1).getThresholds().get(0).getMinThreshold(), 5);
-        assertEquals(user.getAddresses().get(1).getThresholds().get(0).getMaxThreshold(), 20);
+        assertTrue(isInAddressList(user.getAddresses(), "61 Chemin Challet 59800 Lille"));
+        ArrayList<IWeatherThreshold> thresholds = getThresholdsByLocation(user.getAddresses(), "61 Chemin Challet 59800 Lille");
+        assertEquals(thresholds.get(0).getMinThreshold(), 5);
+        assertEquals(thresholds.get(0).getMaxThreshold(), 20);
 
-        assertEquals(user.getAddresses().get(0).getLocation(), "49 rue Reine Elisabeth 06500 Menton");
-        assertEquals(user.getAddresses().get(0).getThresholds().get(0).getMinThreshold(), 3.2);
-        assertEquals(user.getAddresses().get(0).getThresholds().get(0).getMaxThreshold(), 11.5);
+        assertTrue(isInAddressList(user.getAddresses(), "49 rue Reine Elisabeth 06500 Menton"));
+        thresholds = getThresholdsByLocation(user.getAddresses(), "49 rue Reine Elisabeth 06500 Menton");
+        assertEquals(thresholds.get(0).getMinThreshold(), 3.2);
+        assertEquals(thresholds.get(0).getMaxThreshold(), 11.5);
     }
 
     @Test
@@ -69,19 +95,21 @@ public class ConfigurationSystemTest {
         IUser user = userList.get(1);
         assertEquals(user.getName(), "StellarJourney92");
         assertEquals(user.getAddresses().size(), 1);
-        assertEquals(user.getAddresses().get(1).getLocation(), "61 Chemin Challet 59800 Lille");
-        assertEquals(user.getAddresses().get(1).getThresholds().get(0).getMinThreshold(), 5);
-        assertEquals(user.getAddresses().get(1).getThresholds().get(0).getMaxThreshold(), 20);
+        assertTrue(isInAddressList(user.getAddresses(), "61 Chemin Challet 59800 Lille"));
+        ArrayList<IWeatherThreshold> thresholds = getThresholdsByLocation(user.getAddresses(), "61 Chemin Challet 59800 Lille");
+        assertEquals(thresholds.get(0).getMinThreshold(), 5);
+        assertEquals(thresholds.get(0).getMaxThreshold(), 20);
 
         user = userList.get(0);
         assertEquals(user.getName(), "QuantumExplorer");
         assertEquals(user.getAddresses().size(), 1);
-        assertEquals(user.getAddresses().get(0).getLocation(), "65 rue Charles Corbeau 45400 Fleury-les-aubrais");
+        assertTrue(isInAddressList(user.getAddresses(), "65 rue Charles Corbeau 45400 Fleury-les-aubrais"));
         assertEquals(user.getAddresses().get(0).getThresholds().size(), 2);
-        assertEquals(user.getAddresses().get(0).getThresholds().get(0).getMinThreshold(), 10);
-        assertEquals(user.getAddresses().get(0).getThresholds().get(0).getMaxThreshold(), 25);
-        assertEquals(user.getAddresses().get(0).getThresholds().get(1).getMinThreshold(), 1);
-        assertEquals(user.getAddresses().get(0).getThresholds().get(1).getMaxThreshold(), 6);
+        thresholds = getThresholdsByLocation(user.getAddresses(), "65 rue Charles Corbeau 45400 Fleury-les-aubrais");
+        assertEquals(thresholds.get(0).getMinThreshold(), 10);
+        assertEquals(thresholds.get(0).getMaxThreshold(), 25);
+        assertEquals(thresholds.get(1).getMinThreshold(), 1);
+        assertEquals(thresholds.get(1).getMaxThreshold(), 6);
     }
 
 
